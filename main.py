2ff1061a13d1844e36b3e4e7b3da5af50fa0f61c
Okazaki <net.kohei.okazaki@gmail.com>
2023-04-14 21:57:17 +0900

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    pass
+import logging
+from configparser import ConfigParser
+
+import interactions
+from interactions import Client, Intents, listen
+from interactions.ext import prefixed_commands
+
+
+@listen()
+async def on_ready():
+    print("Ready")
+    print(f"This bot is owned by {bot.owner}")
 
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig()
+    cls_log = logging.getLogger(interactions.const.logger_name)
+    cls_log.setLevel(logging.ERROR)
+
+    config = ConfigParser()
+    config.read("config.ini")
+
+    bot = Client(intents=Intents.DEFAULT, sync_interactions=True, asyncio_debug=True, logger=cls_log)
+    prefixed_commands.setup(bot)
+
+    # bot.load_extension("mine_bot")
+    discord_token = config['DISCORD']['token']
+    bot.start(discord_token)
+
